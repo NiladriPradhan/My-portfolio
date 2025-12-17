bdbb957bd1c3868c7f85b79c8941216ab6d3a8e6
niladripradhan <niladrip347@gmail.com>
2025-12-17 17:22:03 +0530

    Fix Turbopack/Webpack conflict for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  webpack: (config: { resolve: { alias: any; }; }) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       "react-native$": "react-native-web",
